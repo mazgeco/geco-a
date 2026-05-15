e3b01bdae3ddc3469559662b579a1910993890fb
Gabby Zapata <gabby.ecuador@gmail.com>
2026-05-15 18:31:47 -0500

    Arreglo del login y comienza módulo Personas

---
+++ src/app/layouts/personas-layout/personas-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-personas-layout',
+  imports: [RouterOutlet],
+  templateUrl: './personas-layout.html'
+})
+export class PersonasLayout {}
